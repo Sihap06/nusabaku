9f48b8635c9a603219d9c2f69f0434280fe29885
unknown <baharuddinsihap@gmail.com>
2020-02-04 00:01:38 +0700

    revisi1

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -9,24 +9,24 @@
 	<link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
 
-	<link rel="stylesheet" href="{{asset('css/open-iconic-bootstrap.min.css')}}">
-	<link rel="stylesheet" href="{{asset('css/animate.css')}}">
+	<link rel="stylesheet" href="{{asset('public/css/open-iconic-bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('public/css/animate.css')}}">
 
-	<link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
-	<link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
-	<link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+	<link rel="stylesheet" href="{{asset('public/css/owl.carousel.min.css')}}">
+	<link rel="stylesheet" href="{{asset('public/css/owl.theme.default.min.css')}}">
+	<link rel="stylesheet" href="{{asset('public/css/magnific-popup.css')}}">
 
-	<link rel="stylesheet" href="{{asset('css/aos.css')}}">
+	<link rel="stylesheet" href="{{asset('public/css/aos.css')}}">
 
-	<link rel="stylesheet" href="{{asset('css/ionicons.min.css')}}">
+	<link rel="stylesheet" href="{{asset('public/css/ionicons.min.css')}}">
 
-	<link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css')}}">
-	<link rel="stylesheet" href="{{asset('css/jquery.timepicker.css')}}">
+	<link rel="stylesheet" href="{{asset('public/css/bootstrap-datepicker.css')}}">
+	<link rel="stylesheet" href="{{asset('public/css/jquery.timepicker.css')}}">
 
 
-	<link rel="stylesheet" href="{{asset('css/flaticon.css')}}">
-	<link rel="stylesheet" href="{{asset('css/icomoon.css')}}">
-	<link rel="stylesheet" href="{{asset('css/style.css')}}">
+	<link rel="stylesheet" href="{{asset('public/css/flaticon.css')}}">
+	<link rel="stylesheet" href="{{asset('public/css/icomoon.css')}}">
+	<link rel="stylesheet" href="{{asset('public/css/style.css')}}">
 
 	<script src="https://kit.fontawesome.com/c024405133.js" crossorigin="anonymous"></script>
 
@@ -52,29 +52,29 @@
 	src="https://code.jquery.com/jquery-3.4.1.js"
 	integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
 	crossorigin="anonymous"></script>
-	<script src="{{asset('js/jquery.min.js')}}"></script>
+	<script src="{{asset('public/js/jquery.min.js')}}"></script>
 
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.js"></script>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/jquery.validate.min.js"></script>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/additional-methods.js"></script>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.1/additional-methods.min.js"></script>
 
-	<script src="{{asset('js/jquery-migrate-3.0.1.min.js')}}"></script>
-	<script src="{{asset('js/popper.min.js')}}"></script>
-	<script src="{{asset('js/bootstrap.min.js')}}"></script>
-	<script src="{{asset('js/jquery.easing.1.3.js')}}"></script>
-	<script src="{{asset('js/jquery.waypoints.min.js')}}"></script>
-	<script src="{{asset('js/jquery.stellar.min.js')}}"></script>
-	<script src="{{asset('js/owl.carousel.min.js')}}"></script>
-	<script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
-	<script src="{{asset('js/aos.js')}}"></script>
-	<script src="{{asset('js/jquery.animateNumber.min.js')}}"></script>
-	<script src="{{asset('js/bootstrap-datepicker.js')}}"></script>
-	<script src="{{asset('js/scrollax.min.js')}}"></script>
+	<script src="{{asset('public/js/jquery-migrate-3.0.1.min.js')}}"></script>
+	<script src="{{asset('public/js/popper.min.js')}}"></script>
+	<script src="{{asset('public/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('public/js/jquery.easing.1.3.js')}}"></script>
+	<script src="{{asset('public/js/jquery.waypoints.min.js')}}"></script>
+	<script src="{{asset('public/js/jquery.stellar.min.js')}}"></script>
+	<script src="{{asset('public/js/owl.carousel.min.js')}}"></script>
+	<script src="{{asset('public/js/jquery.magnific-popup.min.js')}}"></script>
+	<script src="{{asset('public/js/aos.js')}}"></script>
+	<script src="{{asset('public/js/jquery.animateNumber.min.js')}}"></script>
+	<script src="{{asset('public/js/bootstrap-datepicker.js')}}"></script>
+	<script src="{{asset('public/js/scrollax.min.js')}}"></script>
 	{{-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script> --}}
-	<script src="{{asset('js/main.js')}}"></script>
+	<script src="{{asset('public/js/main.js')}}"></script>
 
-	{{-- <script src="{{asset('js/cart.js')}}"></script> --}}
+	{{-- <script src="{{asset('public/js/cart.js')}}"></script> --}}
 	@yield('section-footer')
 	<script>
 		$(document).ready(function(){
